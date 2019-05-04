2c72eb880c982eecdacb885a486121eda3b3c020
liyzcj <liyz0912@gmail.com>
2019-05-04 11:23:30 +0800

    convert to python3

--- Image COCO/convert.py
+++ Image COCO/convert.py
@@ -1,12 +1,17 @@
-import cPickle
+import pickle
 data_Name = "cotra"
 vocab_file = "vocab_" + data_Name + ".pkl"
+vocab_file_py3 = "tmp/vocab_" + data_Name + "_py3.pkl"
 
-word, vocab = cPickle.load(open('save/'+vocab_file))
-print len(word)
-input_file = 'save/generator_sample.txt'
+
+samples_path = './ckpts/test2/samples'
+speech_path = './ckpts/test2/speech'
+
+word, vocab = pickle.load(open(vocab_file_py3, 'rb'))
+print (len(word))
+input_file = samples_path + '/coco_91.txt'
 # input_file = 'save/coco_451.txt'
-output_file = 'speech/' + data_Name + '_' + input_file.split('_')[-1]
+output_file = speech_path + '/' + data_Name + '_' + input_file.split('_')[-1]
 with open(output_file, 'w')as fout:
     with open(input_file)as fin:
         for line in fin:
--- Image COCO/eval_bleu.py
+++ Image COCO/eval_bleu.py
@@ -1,20 +1,29 @@
 import nltk
 import random
 from scipy import stats
-import cPickle
+# import cPickle
+import pickle
 
 
 data_Name = "cotra"
-vocab_file = "vocab_" + data_Name + ".pkl"
+vocab_file = "tmp/vocab_" + data_Name + ".pkl"
+vocab_file_py3 = "tmp/vocab_" + data_Name + "_py3.pkl"
+with open(vocab_file_py3, 'rb') as f:
+    word, vocab = pickle.load(f)
+# with open(vocab_file, 'r') as f:
+#     word, vocab = cPickle.load(f)
 
-word, vocab = cPickle.load(open('save/'+vocab_file))
+# with open(vocab_file_py3, 'wb') as f:
+#     cPickle.dump((word, vocab), f)
 
 pad = vocab[' ']
-print pad
+print (pad)
 
-reference_file = 'save/realtest_coco.txt'
-hypothesis_file_leakgan = 'save/generator_sample.txt'
-# hypothesis_file_leakgan = 'save/coco_451.txt'
+samples_path = './ckpts/test2/samples'
+
+reference_file = 'tmp/realtest_coco.txt'
+# hypothesis_file_leakgan = 'save/generator_sample.txt'
+hypothesis_file_leakgan = samples_path + '/coco_81.txt'
 #################################################
 reference = []
 with open(reference_file)as fin:
@@ -54,4 +63,4 @@ for ngram in range(2,6):
     print ('leakgan')
     print(len(weight), '-gram BLEU score : ', 1.0 * sum(bleu_leakgan) / len(bleu_leakgan))
 
-cPickle.dump([hypothesis_list_leakgan], open('save/significance_test_sample.pkl', 'w'))
+# cPickle.dump([hypothesis_list_leakgan], open('save/significance_test_sample.pkl', 'w'))
